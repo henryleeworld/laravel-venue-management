5a53c76af958626646ecca9da8d6fca8be4d1d85
Henry Lee <henryleeworld@gmail.com>
2019-11-30 11:04:51 +0800

    feat:初始化

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.front')
+
+@section('content')
+    <div class="site-section site-section-sm bg-light">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="site-section-title">
+                        <h2>關於我們</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    由一群不動產經歷豐富多元的經紀人、分析師與業務人員所組成，利用系統性地收集市場資訊並結合大數據分析為基礎，提供專業地產相關顧問服務。
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.front')
+
+@section('content')
+    <div class="site-section site-section-sm bg-light">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-12">
+                    <div class="site-section-title">
+                        <h2>聯絡方式</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12">
+                    地址：台北市信義區光復南路495號6樓之4
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
